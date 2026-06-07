eed2af7be2c9dc9d11efc5d16e11db38c98dfee5
Tran Mai Cong Hung <tranmaiconghung@gmail.com>
2026-06-07 18:48:32 +0700

    fix api of series detail and create

--- src/services/seriesService.js
+++ src/services/seriesService.js
@@ -3,5 +3,17 @@ import { api } from "./api";
 export const seriesService = {
   async getAllSeries() {
     return await await api.get("/Series/get-all-series");
+  },
+  
+  async getAllCategory() {
+    return await await api.get("/Category/get-category");
+  },
+
+  async getSeriesById(seriesId) {
+    return await await api.get(`/Series/get-series-details?seriesId=${seriesId}`);
+  },
+
+  async createSeries(formData) {
+    return await api.post("/Series/create-series", formData);
   }
 };
